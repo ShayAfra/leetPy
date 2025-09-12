a381682cc9ab5b6d2dbbcb443fa094a340072441
unknown <shayafra@gmail.com>
2025-09-11 18:08:25 -0700

    added shortest word py

---
+++ shortestWord.py
@@ -0,0 +1,10 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+def find_short(s):
+    words = s.split()
+    shortestLength= len(words[1])
+    for word in words:
+        if len(word) < shortestLength:
+            shortestLength = len(word)
+    return shortestLength
