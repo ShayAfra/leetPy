b8aecfc7f5ce8e7d0dc2ccc0a104a420aa10424d
ShayAfra <shayafra@gmail.com>
2025-10-08 18:18:41 -0700

    fixed solutionn for stopp spinning my words py

--- StopgninnipSMysdroW.py
+++ StopgninnipSMysdroW.py
@@ -7,4 +7,4 @@
 # "This is another test" --> "This is rehtona test"
 
 def spin_words(sentence):
-    return " ".join(word[::-1] if len(word) > 5 else word for word in sentence.split())
+    return " ".join(word[::-1] if len(word) >= 5 else word for word in sentence.split())
