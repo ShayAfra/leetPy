6f6bd208ec3e8c3ac20fee252868ac96a9752852
ShayAfra <shayafra@gmail.com>
2026-08-19 18:43:24 -0700

    added largest perimeter triangle py

---
+++ largestPerimeterTriangle.py
@@ -0,0 +1,37 @@
+# Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,1,2]
+# Output: 5
+# Explanation: You can form a triangle with three side lengths: 1, 2, and 2.
+# Example 2:
+
+# Input: nums = [1,2,1,10]
+# Output: 0
+# Explanation: 
+# You cannot use the side lengths 1, 1, and 2 to form a triangle.
+# You cannot use the side lengths 1, 1, and 10 to form a triangle.
+# You cannot use the side lengths 1, 2, and 10 to form a triangle.
+# As we cannot use any three side lengths to form a triangle of non-zero area, we return 0.
+ 
+
+# Constraints:
+
+# 3 <= nums.length <= 104
+# 1 <= nums[i] <= 106
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        runSum = [nums[-1],nums[-2],nums[-3]]
+        for i in range(len(nums)-3):
+            if runSum[1] + runSum[2] <= runSum[0]:
+                runSum.pop(0)
+                runSum.append(nums[(len(nums)-4)-i])
+            elif runSum[1] + runSum[2] > runSum[0]:
+                return sum(runSum)
+        if runSum[1] + runSum[2] > runSum[0]:
+            return sum(runSum)
+        return 0
